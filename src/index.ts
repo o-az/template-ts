6e5672d65a2d628751a3a558641daa7172955f2b
Omar <github@omaraziz.dev>
2023-11-24 16:08:37 -0800

    replace nullish co with or

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { server } from '~/server.ts'
 
-export const PORT = Number(process.env.PORT) ?? 3_004
+export const PORT = Number(process.env.PORT) || 3_004
 
 console.log(`Server running at http://localhost:${PORT}`)
 
